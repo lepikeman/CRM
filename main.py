d7e6e21318cad58b773a73358cf53d8c3f28042b
lepikeman <63370994+lepikeman@users.noreply.github.com>
2024-12-10 02:03:51 +0100

    add verification on name and phone number

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import re
+import string
+
 class User:
     def __init__(self, first_name: str, last_name: str, phone_number: str="", address: str=""):
         self.first_name = first_name
@@ -15,6 +18,25 @@ class User:
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
 
+    def _check(self):
+        self._check_phone_number()
+        self._check_names()
+
+    def _check_phone_number(self):
+        phone_digits = re.sub(r"[+()\s]*", "", self.phone_number)
+        if len(phone_digits) < 10 or not phone_digits.isdigit():
+            raise ValueError(f"Numéro de téléphone {self.phone_number} invalide.")
+
+    def _check_names(self):
+        if not (self.first_name and self.last_name):
+            raise ValueError("Le prénom et le nom de famille ne peuvent pas être vides.")
+
+        special_characters = string.punctuation + string.digits
+
+        for character in self.full_name:
+            if character in special_characters:
+                raise ValueError(f"Nom invalide {self.full_name}")
+
 if __name__ == "__main__":
     from faker import Faker
     fake = Faker(locale="fr_FR")
@@ -23,5 +45,6 @@ if __name__ == "__main__":
                     last_name=fake.last_name(),
                     phone_number=fake.phone_number(),
                     address=fake.address())
+        user._check()
         print(user)
         print("-"*10)
